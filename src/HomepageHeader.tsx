c92042e1c2c7489afd68feffefe2f1839acde8e4
Khu Ûi-siông <uisiong.khu@gmail.com>
2025-06-03 22:32:03 +0800

    Fix some problem

--- src/HomepageHeader.tsx
+++ src/HomepageHeader.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LanguageSelector from './components/languageSelector';
 import { useTranslation } from 'react-i18next';
 import { Stack } from 'react-bootstrap';
+import './css/Homepage.css';
 
 
 function HomepageHeader() {
   const {t, i18n} = useTranslation()
+  var titleClass = '';
+  if(i18n.language==='tg_POJ' || i18n.language==='en'){
+    titleClass = 'homepage-title-latin';
+  }else if(i18n.language==='tg_HL' || i18n.language === 'tg_HJ'){
+    titleClass = 'homepage-title-hanji';
+  }
   return (
     <div>
       <Stack direction='horizontal' className='bg-success py-3 ps-1'>
-        <h1 className="homepage-title fs-1 text-white">{t('Title')}</h1> 
+        <h1 className={`${titleClass} fs-1 text-white`} >{t('Title')}</h1> 
         <div className='ms-auto align-buttom'>
           <LanguageSelector />
         </div>
